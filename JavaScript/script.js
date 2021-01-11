d4a86ec83466c563303be1eb899029db535b164c
JP-Grace <gracejohnpaul200@gmail.com>
2021-01-11 16:56:34 -0500

    Added comments and improved README.md

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,6 +2,13 @@
 $(document).ready(function() {
 
     // This checks whether the local storage has the proper values and adds them if not
+    /* This only checks for the "Hour9Text". If "Hour9Text" was there, but a different
+       value was gone, then an hour would occur upon trying to pull from storage.
+       There are a couple of ways that I could fix this. First, I could put all of the
+       values into a single array. I would need to alter the gets and sets in other parts
+       of the code to be compatible with this. Second, I could just check for each item
+       individually. This would be pretty poor looking code, but it would patch the error
+       and allow me to avoid JSON. */
     if(!localStorage.getItem("Hour9Text")) {
 
         localStorage.setItem("Hour9Text", "");
@@ -15,17 +22,24 @@ $(document).ready(function() {
         localStorage.setItem("Hour17Text", "");
     }
 
+    // These lines get the current day and put it into the "currentDay" element
     var currentDay = moment().format("dddd, MMMM Do");
     $("#currentDay").text(currentDay);    
 
+    // A for-loop that pulls from text from local storage and highlights the time blocks
     for (var i = 9; i < 18; i++) {
 
+        // Gets the appropriate textarea as a jQuery object
         var textArea = $("#hour" + i);
-        var currentTime = moment().hour();
 
+        // Gets text from local storage and sets it into the textarea
         var storedText = localStorage.getItem("Hour" + i + "Text");
         textArea.text(storedText);
 
+        // Gets the current time
+        var currentTime = moment().hour();
+
+        // If-statements that highlight the textareas with the past, present, and future classes
         if (currentTime > i) {
             textArea.addClass("past");
         }
@@ -37,13 +51,19 @@ $(document).ready(function() {
         }
     }
 
+    // This is a click listener attached to the container that delegates to the save button
     $(".container").on("click", ".saveBtn", function() {
 
+        // This gets the data-hour attribute from the save button
         var hour = this.dataset.hour;
+
+        // This grabs the text from the corresponding textarea
         var text = $("#hour" + hour).val();
+
+        // This concatenates the local storage value's name
         var storageName = "Hour" + hour + "Text";
 
+        // This sets the local storage value to the text of the corresponding textarea
         localStorage.setItem(storageName, text);
     })
-
 });
